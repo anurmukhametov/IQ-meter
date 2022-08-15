0d3d857b8b1c6ba6e4b0c1c133c89ad9bc5c442f
Arslan <aragon.run@gmail.com>
2022-08-15 16:34:46 +0300

    Upgrade GIF

--- script.js
+++ script.js
@@ -4,34 +4,39 @@ const JENYA = "JENYA";
 const ROMA = "ROMA";
 const CHAGER = "CHAGER";
 
+const highIqSrc = "https://c.tenor.com/hk1_kLT1i5kAAAAd/brain-meme.gif";
+const lowIqSrc =
+	"https://c.tenor.com/dGeybdsH81QAAAAd/guy-singing-opera-opera.gif";
+
 const div = document.createElement("div");
 div.classList.add("gif-container");
 div.id = "gif-container";
-const gif = document.createElement("img");
-gif.src = "https://c.tenor.com/D0MsEp72YkUAAAAd/sueta-oper.gif";
-gif.classList.add("gif");
-gif.alt = "sueta";
-div.appendChild(gif);
 
-var isKyr = function (str) {
+const gif = document.createElement("img");
+gif.classList.add("gif");
+gif.alt = "gif";
+
+function isKyr(str) {
 	return /[а-я]/i.test(str);
-};
+}
+
 function calculateId() {
 	if (input.value === "") {
 		iqLabel.textContent =
 			"Your IQ is 0 because you didn't enter your name!";
+		removeGif();
 	} else if (isKyr(input.value)) {
 		iqLabel.textContent = "Please enter your name in Latin letters!";
+		removeGif();
 	} else if (input.value.length > 15) {
 		iqLabel.textContent = "Sorry your name is too looooong";
+		removeGif();
 	} else {
 		let result;
 		switch (input.value.toUpperCase()) {
 			case JENYA:
 				result = 0;
-				addSueta();
-				iqLabel.textContent = `${input.value} IQ: ${result}`;
-				return;
+				break;
 			case ROMA:
 				result = Number.MAX_SAFE_INTEGER;
 				break;
@@ -43,15 +48,21 @@ function calculateId() {
 				break;
 		}
 		iqLabel.textContent = `${input.value} IQ: ${result}`;
+		if (result >= 100) {
+			addGif(highIqSrc);
+		} else {
+			addGif(lowIqSrc);
+		}
 	}
-	removeSueta();
 }
 
-function addSueta() {
+function addGif(scr) {
+	gif.src = scr;
+	div.appendChild(gif);
 	document.body.appendChild(div);
 }
 
-function removeSueta() {
-	const sueta = document.getElementById("gif-container");
-	if (sueta) sueta.remove();
+function removeGif() {
+	const gif = document.getElementById("gif-container");
+	if (gif) gif.remove();
 }
